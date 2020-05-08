9d5a34e1eab4f8df4f2892ccd2a81d04ce3a04d9
jsonta <56021506+jsonta@users.noreply.github.com>
2020-05-08 21:52:53 +0200

    Obsluga zwracanych bledow, dodatkowe funkcje async

--- Dispatcher/db/resources.js
+++ Dispatcher/db/resources.js
@@ -12,7 +12,10 @@ function getBrigadeInSrvStatus(id) {
             if (results.rowCount > 0) {
                 resolve(results.rows[0].in_service);
             } else {
-                reject(404);
+                reject({
+                    errno: 404,
+                    code: "Not Found"
+                });
             }
         });
     });
@@ -28,25 +31,35 @@ function getBusInSrvStatus(id) {
             if (results.rowCount > 0) {
                 resolve(results.rows[0].in_service);
             } else {
-                reject(404);
+                reject({
+                    errno: 404,
+                    code: "Not Found"
+                });
+            }
+        });
+    });
+}
+function getDriverInSrvStatus(id) {
+    return new Promise((resolve, reject) => {
+        pool.query('SELECT in_service FROM drivers WHERE id = $1', [id],
+        (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+
+            if (results.rowCount > 0) {
+                resolve(results.rows[0].in_service);
+            } else {
+                reject({
+                    errno: 404,
+                    code: "Not Found"
+                });
             }
         });
     });
 }
 
 // Aktualizacja informacji w bazie danych
-function updBusInSrvStatus(id, status) {
-    return new Promise((resolve, reject) => {
-        pool.query('UPDATE buses SET in_service = $2 WHERE id = $1',
-        [id, status], (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-
-            resolve(200);
-        });
-    });
-}
 function updBrigadeInSrvStatus(id, status) {
     return new Promise((resolve, reject) => {
         pool.query('UPDATE brigades SET in_service = $2 WHERE id = $1',
@@ -55,43 +68,226 @@ function updBrigadeInSrvStatus(id, status) {
                 return reject(err);
             }
 
-            resolve(200);
+            resolve(results);
+        });
+    });
+}
+function updBusInSrvStatus(id, status) {
+    return new Promise((resolve, reject) => {
+        pool.query('UPDATE buses SET in_service = $2 WHERE id = $1',
+        [id, status], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(results);
+        });
+    });
+}
+function updDriverInSrvStatus(id, status) {
+    return new Promise((resolve, reject) => {
+        pool.query('UPDATE drivers SET in_service = $2 WHERE id = $1',
+        [id, status], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve(results);
         });
     });
 }
 
-// Zmiana wartosci 'in_service' - funkcje asynchroniczne
-var flipBusInSrvStatus = async function(request, response) {
-    const id = parseInt(request.params.id);
-    var status;
-
-    await getBusInSrvStatus(id)
-            .then((in_service) => {
-                status = !!in_service;
-            })
-            .catch(err => response.sendStatus(parseInt(err)));
-
-    status = !status;
-    await updBusInSrvStatus(id, status).then((statusCode) => {
-        response.sendStatus(parseInt(statusCode));
-    });
-}
-var flipBrigadeInSrvStatus = async function(request, response) {
-    const id = request.params.id;
-    var status;
+// Odczyt informacji z bazy danych - funkcje asynchroniczne
+var readBrigadeInSrvStatus = async function(request, response) {
+    let id = request.params.id;
 
     await getBrigadeInSrvStatus(id)
-            .then((in_service) => {
-                status = !!in_service;
-            })
-            .catch(err => response.sendStatus(parseInt(err)));
+    .then((in_service) => {
+        response.status(200).send(in_service);
+    })
+    .catch((err) => {
+        let errNo = parseInt(err.errno);
 
-    status = !status;
-    await updBrigadeInSrvStatus(id, status).then((statusCode) => {
-        response.sendStatus(parseInt(statusCode));
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
     });
+}
+var readBusInSrvStatus = async function(request, response) {
+    let id = request.params.id;
+
+    await getBusInSrvStatus(id)
+    .then((in_service) => {
+        response.status(200).send(in_service);
+    })
+    .catch((err) => {
+        let errNo = parseInt(err.errno);
+
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
+    });
+}
+var readDriverInSrvStatus = async function(request, response) {
+    let id = request.params.id;
+
+    await getDriverInSrvStatus(id)
+    .then((in_service) => {
+        response.status(200).send(in_service);
+    })
+    .catch((err) => {
+        let errNo = parseInt(err.errno);
+
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
+    });
+}
+
+// Zmiana wartosci 'in_service' - funkcje asynchroniczne
+var flipBrigadeInSrvStatus = async function(request, response) {
+    let id = request.params.id;
+    let errReturned = false;
+    let status;
+
+    await getBrigadeInSrvStatus(id)
+    .then((in_service) => {
+        status = !!in_service;
+    })
+    .catch((err) => {
+        errReturned = !errReturned;
+        let errNo = parseInt(err.errno);
+
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
+    });
+
+    if (!errReturned) {
+        status = !status;
+        
+        await updBrigadeInSrvStatus(id, status)
+        .then(() => {
+            response.sendStatus(200);
+        })
+        .catch((err) => {
+            let errNo = parseInt(err.errno);
+
+            if (errNo >= 400) {
+                response.status(errNo)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            } else {
+                response.status(400)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            }
+        });
+    }
+}
+var flipBusInSrvStatus = async function(request, response) {
+    let id = parseInt(request.params.id);
+    let errReturned = false;
+    let status;
+
+    await getBusInSrvStatus(id)
+    .then((in_service) => {
+        status = !!in_service;
+    })
+    .catch((err) => {
+        errReturned = !errReturned;
+        let errNo = parseInt(err.errno);
+
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
+    });
+
+    if (!errReturned) {
+        status = !status;
+
+        await updBusInSrvStatus(id, status)
+        .then(() => {
+            response.sendStatus(200);
+        })
+        .catch((err) => {
+            let errNo = parseInt(err.errno);
+
+            if (errNo >= 400) {
+                response.status(errNo)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            } else {
+                response.status(400)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            }
+        });
+    }
+}
+var flipDriverInSrvStatus = async function(request, response) {
+    let id = parseInt(request.params.id);
+    let errReturned = false;
+    let status;
+
+    await getDriverInSrvStatus(id)
+    .then((in_service) => {
+        status = !!in_service;
+    })
+    .catch((err) => {
+        errReturned = !errReturned;
+        let errNo = parseInt(err.errno);
+
+        if (errNo >= 400) {
+            response.status(errNo)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        } else {
+            response.status(400)
+                    .send("Error: "+err.code+" ("+errNo+")");
+        }
+    });
+
+    if (!errReturned) {
+        status = !status;
+
+        await updDriverInSrvStatus(id, status)
+        .then(() => {
+            response.sendStatus(200);
+        })
+        .catch((err) => {
+            let errNo = parseInt(err.errno);
+
+            if (errNo >= 400) {
+                response.status(errNo)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            } else {
+                response.status(400)
+                        .send("Error: "+err.code+" ("+errNo+")");
+            }
+        });
+    }
 }
 
 // Eksport funkcji asynchronicznych do uzytku na zewnatrz
+exports.readBrigadeInSrvStatus = readBrigadeInSrvStatus;
+exports.readBusInSrvStatus = readBusInSrvStatus;
+exports.readDriverInSrvStatus = readDriverInSrvStatus;
 exports.flipBrigadeInSrvStatus = flipBrigadeInSrvStatus;
 exports.flipBusInSrvStatus = flipBusInSrvStatus;
+exports.flipDriverInSrvStatus = flipDriverInSrvStatus;
--- Dispatcher/routes.js
+++ Dispatcher/routes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 const resources = require('./db/resources.js');
 
+const readBrigade = resources.readBrigadeInSrvStatus;
+const readBus = resources.readBusInSrvStatus;
+const readDriver = resources.readDriverInSrvStatus;
 const flipBrigade = resources.flipBrigadeInSrvStatus;
 const flipBus = resources.flipBusInSrvStatus;
+const flipDriver = resources.flipDriverInSrvStatus;
 
-router.get('/flipBusInSrvStatus/:id', function(req, res) { flipBus(req, res) });
+router.get('/readBrigadeInSrvStatus/:id', function(req, res) { readBrigade(req, res) });
+router.get('/readBusInSrvStatus/:id', function(req, res) { readBus(req, res) });
+router.get('/readDriverInSrvStatus/:id', function(req, res) { readDriver(req, res) });
 router.get('/flipBrigadeInSrvStatus/:id', function(req, res) { flipBrigade(req, res) });
+router.get('/flipBusInSrvStatus/:id', function(req, res) { flipBus(req, res) });
+router.get('/flipDriverInSrvStatus/:id', function(req, res) { flipDriver(req, res) });
 
 module.exports = router;
